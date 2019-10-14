b46edae58a3bfe5ff7df88e7807890911013acba
ravikantpal1408 <ravikantpal123@gmail.com>
2019-10-14 13:01:11 +0530

    created transfer migrations

--- MicroRabbit.Transfer.API/Startup.cs
+++ MicroRabbit.Transfer.API/Startup.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
+using MicroRabbit.Transfer.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MicroRabbit.Transfer.API
 {
@@ -25,7 +31,26 @@ namespace MicroRabbit.Transfer.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TransferDbContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("TransferDbConnection"));
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info {Title = "Transfer Microservices", Version = "v1"});
+            });
+
+            services.AddMediatR(typeof(Startup));
+
+            RegisterServices(services);
+        }
+        
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +67,11 @@ namespace MicroRabbit.Transfer.API
             }
 
             app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Transfer Microservices V1");
+            });
             app.UseMvc();
         }
     }
---
+++ MicroRabbit.Transfer.Domain/Models/TransferLog.cs
@@ -0,0 +1,11 @@
+namespace MicroRabbit.Transfer.Domain.Models
+{
+    public class TransferLog
+    {
+        public int Id { get; set; }
+        public int FromAccount { get; set; }
+        public int ToAccount { get; set; }
+        public decimal TransferAccount { get; set; }
+        
+    }
+}
